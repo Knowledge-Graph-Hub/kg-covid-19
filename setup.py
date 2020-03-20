77cbcb1ee81778420ec61f5e1694dd22b238322d
Luca Cappelletti <cappelletti.luca94@gmail.com>
2020-03-20 17:24:15 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -59,6 +59,9 @@ setup(
     packages=find_packages(exclude=['contrib', 'docs', 'tests*']),
     tests_require=test_deps,
     # Add here the package dependencies
-    install_requires=[],
+    install_requires=[
+        "tqdm",
+        "encodeproject"
+    ],
     extras_require=extras,
 )
