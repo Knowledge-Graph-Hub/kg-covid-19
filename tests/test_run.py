9f5c5c2ac7cf65cde67f99b56940f9ea232e5ed3
Justin Reese <justaddcoffee+github@gmail.com>
2020-03-24 10:04:19 -0700

    Fix gitignore issue

--- tests/test_run.py
+++ tests/test_run.py
@@ -6,6 +6,6 @@ from kg_emerging_viruses import download, transform
 def test_run():
     """Tests the run.py script."""
 
-    download('tests/data/download.yaml', 'tests/data/raw')
+    download('tests/resources/download.yaml', 'tests/data/raw')
 
     return None
