acfb506006475c612fcf9308b50855a9811d7992
Justin Reese <justaddcoffee+github@gmail.com>
2020-03-24 15:59:12 -0700

    Better test for run.py

--- tests/test_run.py
+++ tests/test_run.py
@@ -1,11 +1,25 @@
-import os
+from unittest import TestCase, skip
+from click.testing import CliRunner
 
-from kg_emerging_viruses import download, transform
+import kg_emerging_viruses
+
+from run import transform
+from run import download
 
 
-def test_run():
+class TestRun(TestCase):
     """Tests the run.py script."""
+    def setUp(self) -> None:
+        self.runner = CliRunner()
 
-    download('tests/resources/download.yaml', 'tests/data/raw')
+    def test_download(self):
+        result = self.runner.invoke(cli=download,
+                                    args=['-y', 'tests/resources/download.yaml'])
+        self.assertEqual(result.exit_code, 0)
 
-    return None
+    @skip
+    def test_transform(self):
+        result = self.runner.invoke(cli=transform,
+                                    args=['-i', 'tests/data/raw'])
+        self.assertEqual(result.exit_code, 0)
+
