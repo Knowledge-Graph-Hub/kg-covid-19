c863db9b0957bce2897bdb07288a28effda9e022
Justin Reese <justaddcoffee+github@gmail.com>
2020-03-25 14:31:09 -0700

    Add test helper that mocks requests.get, and add test in test_run.py that checks that requests.url was called

--- tests/test_run.py
+++ tests/test_run.py
@@ -1,11 +1,14 @@
+import requests
+import kg_emerging_viruses
+
 from unittest import TestCase, skip
 from click.testing import CliRunner
-from mock import patch
-
-import kg_emerging_viruses
+from unittest import mock
+from kg_emerging_viruses import download
 
 from run import transform
 from run import download
+from tests.resources.mock_download import mocked_requests_get
 
 
 class TestRun(TestCase):
@@ -13,20 +16,16 @@ class TestRun(TestCase):
     def setUp(self) -> None:
         self.runner = CliRunner()
 
-    @skip
-    def test_download(self):
-        # TODO: I cannot for the life of me convince mock/patch to intercept call
-        # to kg_download() in run.download()
+    @mock.patch('requests.get', side_effect=mocked_requests_get)
+    def test_download(self, mock_get):
         result = self.runner.invoke(cli=download,
-                                    args=['-y', 'tests/resources/download.yaml'])
-        self.assertTrue(kg_emerging_viruses.download.called, True)
-        self.assertEqual(result.exit_code, 0)
+                                    args=['-y', 'resources/download.yaml'])
+        # this really just makes sure request.get get called somewhere downstream
+        self.assertTrue(mock_get.called)
 
     @skip
     def test_transform(self):
-        # TODO: Likewise, need to mock/path kg_transform() in run.transform()
         result = self.runner.invoke(cli=transform,
-                                    args=['-i', 'tests/data/raw'])
-        self.assertTrue(kg_emerging_viruses.transform.called, True)
+                                    args=['-i', 'data/raw'])
         self.assertEqual(result.exit_code, 0)
 
