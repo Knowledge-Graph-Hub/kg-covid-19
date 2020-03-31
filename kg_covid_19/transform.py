7df92eb5320b5bb3dc2b6a15bae51baa85eeba80
Deepak Unni <deepak.unni3@gmail.com>
2020-03-30 17:57:15 -0700

    Add option to specify which source to transform
    Add an option to transform command to specify
    one or more sources to transform.
    If no value is provided then all sources
    are transformed.

--- kg_covid_19/transform.py
+++ kg_covid_19/transform.py
@@ -1,14 +1,19 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+from typing import List
 
-
-from kg_covid_19.transform_utils import zhou_transform
 from kg_covid_19.transform_utils.drug_central.drug_central import DrugCentralTransform
 from kg_covid_19.transform_utils.string_ppi import StringTransform
 from kg_covid_19.transform_utils.zhou_host_proteins.zhou_transform import ZhouTransform
 
 
-def transform(input_dir: str, output_dir: str) -> None:
+DATA_SOURCES = {
+    'ZhouTransform': ZhouTransform,
+    'DrugCentralTransform': DrugCentralTransform,
+    'StringTransform': StringTransform
+}
+
+def transform(input_dir: str, output_dir: str, sources: List = None) -> None:
     """Call scripts in kg_covid_19/transform/[source name]/ to transform each source into a graph format that
     KGX can ingest directly, in either TSV or JSON format:
     https://github.com/NCATS-Tangerine/kgx/blob/master/data-preparation.md
@@ -16,17 +21,20 @@ def transform(input_dir: str, output_dir: str) -> None:
     Args:
         input_dir: A string pointing to the directory to import data from.
         output_dir: A string pointing to the directory to output data to.
+        sources: A list of sources to transform.
 
     Returns:
         None.
     """
+    if not sources:
+        # run all sources
+        sources = DATA_SOURCES.keys()
 
-    # call transform script for each source
-    zt = ZhouTransform()
-    zt.run()
-
-    dct = DrugCentralTransform()
-    dct.run()
-
-    string_ppi = StringTransform(input_dir, output_dir)
-    string_ppi.run()
+    for source in sources:
+        print(source)
+        if source in DATA_SOURCES:
+            if source == 'StringTransform':
+                t = DATA_SOURCES[source](input_dir, output_dir)
+            else:
+                t = DATA_SOURCES[source]()
+            t.run()
