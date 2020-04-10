97598ad8394560084a12dadc59e8aaa05e46a3b9
Deepak Unni <deepak.unni3@gmail.com>
2020-04-10 15:56:38 -0700

    Disable transformation of co-occurrences until upstream data is updated

--- kg_covid_19/transform_utils/scibite_cord/scibite_cord.py
+++ kg_covid_19/transform_utils/scibite_cord/scibite_cord.py
@@ -53,7 +53,7 @@ class ScibiteCordTransform(Transform):
         node_handle.write("\t".join(self.node_header) + "\n")
         edge_handle.write("\t".join(self.edge_header) + "\n")
         self.parse_annotations(node_handle, edge_handle, data_files[0])
-        self.parse_cooccurrence(node_handle, edge_handle, data_files[1])
+        #self.parse_cooccurrence(node_handle, edge_handle, data_files[1])
 
     def parse_annotations(self, node_handle: Any, edge_handle: Any, data_file: str) -> None:
         """Parse annotations from CORD-19_1_2.zip.
