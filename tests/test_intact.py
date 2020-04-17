e260c29b028c1473716d6f4f9fe9339388d86019
Justin Reese <justaddcoffee+github@gmail.com>
2020-04-17 14:58:52 -0700

    Refactor

--- tests/test_intact.py
+++ tests/test_intact.py
@@ -16,7 +16,9 @@ class TestIntAct(unittest.TestCase):
         self.assertTrue(isinstance(parsed['edges'], list))
 
     def test_nodes_parse_xml_to_nodes_edges(self):
-        (nodes, _) = self.intact.parse_xml_to_nodes_edges(self.xml_file)
+        parsed = self.intact.parse_xml_to_nodes_edges(self.xml_file)
+        self.assertEqual(len(parsed['nodes']), 5,
+                         "Didn't get the expected number of nodes")
         # <shortLabel>btf3_human</shortLabel>
         # <primaryRef db="uniprotkb" dbAc="MI:0486" id="P20290" version="SP_65" refType="identity" refTypeAc="MI:0356"/>
         # <shortLabel>nsp10_cvhsa</shortLabel>
@@ -28,10 +30,10 @@ class TestIntAct(unittest.TestCase):
         # <primaryRef db="uniprotkb" dbAc="MI:0486" id="P00403" version="SP_95" refType="identity" refTypeAc="MI:0356"/>
         # <shortLabel>atf5_human</shortLabel>
         # <primaryRef db="uniprotkb" dbAc="MI:0486" id="Q9Y2D1" version="SP_67" refType="identity" refTypeAc="MI:0356"/>
-        self.assertEqual(len(nodes), 5, "Didn't get the expected number of nodes")
+
 
     @unittest.skip
     def test_edges_parse_xml_to_nodes_edges(self):
-        (_, edges) = self.intact.parse_xml_to_nodes_edges(self.xml_file)
+        parsed = self.intact.parse_xml_to_nodes_edges(self.xml_file)
         self.assertTrue(False)
 
