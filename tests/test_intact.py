0557b82539b3e5794e09ee50b032a8b2dd9563c6
Justin Reese <justaddcoffee+github@gmail.com>
2020-04-18 12:57:27 -0700

    Test refactor

--- tests/test_intact.py
+++ tests/test_intact.py
@@ -33,7 +33,7 @@ class TestIntAct(unittest.TestCase):
         self.assertTrue('edges' in parsed)
         self.assertEqual(len(parsed['edges']), 8,
                          "Didn't get the expected number of edges")
-        self.assertCountEqual(parsed['edges'][0],
+        self.assertCountEqual(parsed['edges'][0][:4],
                               ['UniProtKB:P20290',
                                'biolink:interacts_with',
                                'UniProtKB:P0C6X7-PRO_0000037317',
