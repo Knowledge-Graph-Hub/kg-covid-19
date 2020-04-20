070d5ee507c4215b99b924b5d682039c7c79251e
Justin Reese <justaddcoffee+github@gmail.com>
2020-04-20 11:05:52 -0700

    Fixed slightly incorrect test - assertCountEqual() should be assertEqual()

--- tests/test_intact.py
+++ tests/test_intact.py
@@ -7,6 +7,8 @@ class TestIntAct(unittest.TestCase):
 
     def setUp(self) -> None:
         self.xml_file = 'tests/resources/intact_test.xml'
+        self.chebi_test_xml = \
+            'tests/resources/31315999_weird_chebi_id.xml'  # weird odd CHEBI id
         self.intact = IntAct()
 
     def test_struct_parse_xml_to_nodes_edges(self):
@@ -20,11 +22,11 @@ class TestIntAct(unittest.TestCase):
         self.assertTrue('nodes' in parsed)
         self.assertEqual(len(parsed['nodes']), 5,
                          "Didn't get the expected number of nodes")
-        self.assertCountEqual(parsed['nodes'][0],
+        self.assertEqual(parsed['nodes'][0],
                               ['UniProtKB:P20290', 'btf3_human', 'biolink:Protein'])
         # one xml contains an RNA interaction, so let's test that
         # we parse that correctly
-        self.assertCountEqual(parsed['nodes'][1],
+        self.assertEqual(parsed['nodes'][1],
                               ['UniProtKB:P0C6X7-PRO_0000037317', 'nsp10_cvhsa',
                                'biolink:RNA'])
 
@@ -33,7 +35,7 @@ class TestIntAct(unittest.TestCase):
         self.assertTrue('edges' in parsed)
         self.assertEqual(len(parsed['edges']), 8,
                          "Didn't get the expected number of edges")
-        self.assertCountEqual(parsed['edges'][0][:4],
+        self.assertEqual(parsed['edges'][0][:4],
                               ['UniProtKB:P20290',
                                'biolink:interacts_with',
                                'UniProtKB:P0C6X7-PRO_0000037317',
