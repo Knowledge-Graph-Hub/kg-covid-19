114142bed4f5c47f41fb91fd6c3bd86bd67e6ae9
Justin Reese <justaddcoffee+github@gmail.com>
2020-05-13 12:49:15 -0700

    Code/tests to add PRO_ proteins to candidate list

--- kg_covid_19/query_utils/target_candidates/target_candidates.py
+++ kg_covid_19/query_utils/target_candidates/target_candidates.py
@@ -1,6 +1,7 @@
 import builtins
 import logging
 import os
+import re
 import tarfile
 from typing import List, Union
 
@@ -43,7 +44,7 @@ class TargetCandidates(Query):
         self.sars_cov_2_edges = os.path.join('data', "transformed",
                                              "sars_cov_2_gene_annot/edges.tsv")
 
-    def run(self):
+    def run(self) -> None:
         # extract TSV files
         tar = tarfile.open(self.merged_tsv_tarfile, "r:gz")
         tar.extractall(path=os.path.join("data", "merged"))
@@ -51,24 +52,90 @@ class TargetCandidates(Query):
 
         candidates: list = []
 
-        # include SARS-CoV-2 proteins from gene annotation transform
+        # read in data files
         sars_cov2_df = pd.read_csv(self.sars_cov_2_nodes, sep="\t")
+        merged_edges_df = pd.read_csv(os.path.join(self.input_dir,
+                                                   self.merged_edges_file),
+                                      sep='\t')
+        merged_nodes_df = pd.read_csv(os.path.join(self.input_dir,
+                                                   self.merged_nodes_file),
+                                      sep='\t')
+
+        # add SARS-CoV-2 proteins from gene annotation transform
         candidates.extend(
             self.sars_cov2_to_candidate_entries(sars_cov2_df,
                                               'V', 'id', 'name', 1,
                                               "annotated SARS-CoV-2 gene"))
+        sars_cov2_ids = [c[1] for c in candidates]
 
-        merged_nodes_df = pd.read_csv(os.path.join(self.input_dir,
-                                                   self.merged_nodes_file),
-                                      sep='\t')
-        merged_edges_df = pd.read_csv(os.path.join(self.input_dir,
-                                                   self.merged_nodes_file),
-                                      sep='\t')
-        # generate list of proteins that interact with SARS-CoV-2 according to IntAct
+        # append PRO_ SARS-CoV-2 proteins
+        candidates.extend(
+            self.sars_cov2_pro_candidates(sars_cov2_ids,
+                                          sars_cov2_df,
+                                          'V', 'id', 'name', 1,
+                                          "annotated SARS-CoV-2 gene"))
 
 
+        all_sars_cov2_ids = [c[1] for c in candidates]
 
-        pass
+
+        # append list of proteins that interact with SARS-CoV-2 according to IntAct
+        candidates.extend(
+        self.sars_cov2_and_intact_to_candidate_entries(all_sars_cov2_ids,
+                                                       ['intact'],
+                                                       merged_edges_df,
+                                                       merged_nodes_df,
+                                                       "H",
+                                                       ['subject', 'object'],
+                                                       ['name'],
+                                                       0.75,
+                                                       'inferred from intact')
+        )
+
+        self.candidates_to_tsv(candidates, self.output_dir, outfile_name)
+        # import csv
+        # data = ['text1', 'text2', 'text3', 'text4']
+        # with open('output.tsv', 'w', newline='') as f_output:
+        #     tsv_output = csv.writer(f_output, delimiter='\t')
+        #     tsv_output.writerow(data)
+
+    def sars_cov2_pro_candidates(self,
+                                 these_ids: list,
+                                 sars_cov2_df,
+                                 viral_or_host: str,
+                                 id_col: str,
+                                 name_col: str,
+                                 confidence_score: str,
+                                 comments: str):
+        """Given sars-cov-2 gene annotations, extract and make candidate entry
+        :param IDs for which to look in nodes df for PRO_ genes
+        :param sars_cov2_df:
+        :param viral_or_host:
+        :param id_col:
+        :param name_col:
+        :param confidence_score:
+        :param comments:
+
+        :return: List - candidate entry in format described in docstring
+        """
+        candidate_entries: list = []
+
+        all_ids = list(sars_cov2_df[id_col])
+        all_names = list(sars_cov2_df[name_col])
+        for id_to_match in these_ids:
+            this_re = re.compile(id_to_match)
+            matching_indices = [i for i, item in enumerate(all_ids) if this_re.search(item)]
+
+            for this_idx in matching_indices:
+                this_id = all_ids[this_idx]
+                this_name = all_names[this_idx]
+
+                if this_id == id_to_match:  # don't match identical IDs
+                    continue
+                candidate_entry = [viral_or_host, this_id, this_name, confidence_score, comments]
+                candidate_entries.append(candidate_entry)
+        return candidate_entries
+
 
     def sars_cov2_to_candidate_entries(self,
                                        sars_cov2_df,
@@ -97,4 +164,44 @@ class TargetCandidates(Query):
             if name_col in row:
                 candidate_entry[2] = row[name_col]
             candidate_entries.append(candidate_entry)
+        return candidate_entries
+
+
+    def sars_cov2_and_intact_to_candidate_entries(self,
+                                                  sars_cov2_ids: list,
+                                                  provided_by: list,
+                                                  edge_df,
+                                                  nodes_df,
+                                                  viral_or_host: str,
+                                                  id_cols: list,
+                                                  name_col: str,
+                                                  confidence_score: str,
+                                                  comments: str
+                                                  ) -> List[List[Union[str, int]]]:
+        """Given sars-cov-2 genes and a pandas DF with edge data and node data, extract
+        all proteins that interact with these sars-cov2 genes according to source(s) in
+        'provided_by'
+
+        :param sars_cov2_ids
+        :param provided_by
+        :param edge_df
+        :param node_df
+        :param viral_or_host:
+        :param id_col:
+        :param edge_df,
+        :param comments:
+        :param confidence_score:
+        :param name_col:
+        :param sars_cov2_df:
+        :return: List - candidate entry in format described in docstring
+        """
+        candidate_entries: list = []
+
+        # for _, row in sars_cov2_df.iterrows():
+        #     candidate_entry = [viral_or_host, None, None, confidence_score, comments]
+        #     if id_col in row:
+        #         candidate_entry[1] = row[id_col]
+        #     if name_col in row:
+        #         candidate_entry[2] = row[name_col]
+        #     candidate_entries.append(candidate_entry)
         return candidate_entries
--- tests/test_targetCandidates.py
+++ tests/test_targetCandidates.py
@@ -28,3 +28,24 @@ class TestTargetCandidates(TestCase):
                           1,
                           "annotated SARS-CoV-2 gene"],
                          candidates[0])
+
+    def test_sars_cov2_pro_candidates(self):
+        sars_cov_df = pd.read_csv(
+            "tests/test_sars_cov2_pro_candidates_nodes_SNIPPET.tsv", sep="\t")
+
+        candidates = self.tc.sars_cov2_pro_candidates(
+                                                        ["UniProtKB:P0DTC1"],
+                                                        sars_cov_df,
+                                                        'V', 'id', 'name', 1,
+                                                        "annotated SARS-CoV-2 gene")
+        self.assertEqual(1, len(candidates))
+        self.assertEqual(
+                         ["V",
+                          "UniProtKB:P0DTC1-PRO_0000449645",
+                          "p0dtc1-pro_0000449645",
+                          1,
+                          "annotated SARS-CoV-2 gene"],
+                         candidates[0])
+
+    def test_sars_cov2_and_intact_to_candidate_entries(self):
+        self.assertTrue(hasattr(self.tc, 'sars_cov2_and_intact_to_candidate_entries'))
