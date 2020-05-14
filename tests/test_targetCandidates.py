bebd9560000b73990401b5a42cc0b0782a694fa0
Justin Reese <justaddcoffee+github@gmail.com>
2020-05-13 17:43:07 -0700

    Failing test for getting IntAct SARS-CoV-2 proteins

--- tests/test_targetCandidates.py
+++ tests/test_targetCandidates.py
@@ -49,8 +49,28 @@ class TestTargetCandidates(TestCase):
                           "annotated SARS-CoV-2 gene"],
                          candidates[0])
 
+    def test_sars_cov2_in_intact_set_to_candidate_entries(self):
+        self.assertTrue(hasattr(self.tc,
+                                'sars_cov2_in_intact_set_to_candidate_entries'))
+        existing_ids = ['UniProtKB:P0DTC2']  # don't want these again
+        nodes_df = pd.read_csv("tests/resources/P0DTC1.nodes.tsv", sep="\t")
+        candidates = self.tc.sars_cov2_in_intact_set_to_candidate_entries(
+            existing_ids=existing_ids,
+            taxon_id='2697049',
+            nodes_df=nodes_df
+        )
+        self.assertEqual(1, len(candidates))
+        self.assertEqual(
+                         ['V',
+                          'UniProtKB:P0DTD1-PRO_0000449619',
+                          'nsp1_wcpv',
+                          1,
+                          'present in intact database'],
+                         candidates[0])
+
     def test_sars_cov2_and_intact_to_candidate_entries(self):
-        self.assertTrue(hasattr(self.tc, 'sars_cov2_and_intact_to_candidate_entries'))
+        self.assertTrue(hasattr(self.tc,
+                                'sars_cov2_human_interactors_to_candidate_entries'))
         edges_df = pd.read_csv("tests/resources/P0DTC1.edges.tsv", sep="\t")
         nodes_df = pd.read_csv("tests/resources/P0DTC1.nodes.tsv", sep="\t")
 
