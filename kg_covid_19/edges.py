d116035bb813f584e8cb849f82f9683ae5addefe
Justin Reese <justaddcoffee+github@gmail.com>
2020-05-14 10:00:04 -0700

    Add edgespy

---
+++ kg_covid_19/edges.py
@@ -0,0 +1,13 @@
+def make_edges(input_dir: str, output_dir: str) -> None:
+    """Prepare positive and negative edges for testing and training
+
+    Args:
+        input_dir: A string pointing to the directory to import data from.
+        output_dir: A string pointing to the directory to output data to.
+        sources: A list of sources to transform.
+
+    Returns:
+        None.
+
+    """
+    pass
