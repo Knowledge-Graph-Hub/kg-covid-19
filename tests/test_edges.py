d417eb7b9f997498588b4f7c7b9fbd9e4ad93ffd
Justin Reese <justaddcoffee+github@gmail.com>
2020-05-15 22:14:24 -0700

    Break tests into units

--- tests/test_edges.py
+++ tests/test_edges.py
@@ -8,9 +8,19 @@ from kg_covid_19.edges import make_edges, tsv_to_df, has_disconnected_nodes, \
 
 
 class TestEdges(unittest.TestCase):
+    @classmethod
+    def setUpClass(cls) -> None:
+        cls.small_nodes_file = 'tests/resources/edges/small_graph_nodes.tsv'
+        cls.small_edges_file = 'tests/resources/edges/small_graph_edges.tsv'
+        cls.edges = tsv_to_df(cls.small_edges_file)
+        cls.nodes = tsv_to_df(cls.small_nodes_file)
+
+        # make neg edges for small graph
+        cls.num_edges = 5
+        cls.neg_edge_df = make_negative_edges(cls.num_edges, cls.nodes, cls.edges)
+
     def setUp(self) -> None:
-        self.small_nodes_file = 'tests/resources/edges/small_graph_nodes.tsv'
-        self.small_edges_file = 'tests/resources/edges/small_graph_edges.tsv'
+        pass
 
     def test_tsv_to_df(self):
         df = tsv_to_df(self.small_edges_file)
@@ -34,33 +44,34 @@ class TestEdges(unittest.TestCase):
         self.assertTrue(has_disconnected_nodes(edges_df=edges,
                                                nodes_df=nodes_extra_ids))
 
-    def test_make_negative_edges(self):
-        num_edges = 5
+    def test_make_negative_edges_check_instance_type(self):
+        self.assertTrue(isinstance(self.neg_edge_df, pd.DataFrame))
+
+    def test_make_negative_edges_check_num_edges_returned(self):
+        self.assertEqual(self.num_edges, self.neg_edge_df.shape[0])
+
+    def test_make_negative_edges_columns(self):
         expected_columns = ['subject', 'edge_label', 'object', 'relation']
         expected_edge_label = 'negative_edge'
         expected_relation = 'negative_edge'
-        edges = tsv_to_df(self.small_edges_file)
-        nodes = tsv_to_df(self.small_nodes_file)
-        unique_node_ids = list(np.unique(nodes.id))
-
-        neg_edge_df = make_negative_edges(num_edges, nodes, edges)
-        self.assertTrue(isinstance(neg_edge_df, pd.DataFrame))
-        self.assertEqual(num_edges, neg_edge_df.shape[0])
-        self.assertEqual(len(expected_columns), neg_edge_df.shape[1],
+        self.assertEqual(len(expected_columns), self.neg_edge_df.shape[1],
                          "didn't get expected columns in negative edge df")
-        self.assertListEqual(expected_columns, list(neg_edge_df.columns))
-        self.assertListEqual([expected_edge_label] * neg_edge_df.shape[0],
-                             list(neg_edge_df.edge_label),
+        self.assertListEqual(expected_columns, list(self.neg_edge_df.columns))
+        self.assertListEqual([expected_edge_label] * self.neg_edge_df.shape[0],
+                             list(self.neg_edge_df.edge_label),
                              "Edge label column not correct")
-        self.assertListEqual([expected_relation] * neg_edge_df.shape[0],
-                             list(neg_edge_df.relation),
+        self.assertListEqual([expected_relation] * self.neg_edge_df.shape[0],
+                             list(self.neg_edge_df.relation),
                              "Relation column not correct")
 
-        neg_nodes = list(np.unique(np.concatenate((neg_edge_df.subject,
-                                                   neg_edge_df.object))))
+    def test_make_negative_edges_check_neg_nodes(self):
+        unique_node_ids = list(np.unique(self.nodes.id))
+        neg_nodes = list(np.unique(np.concatenate((self.neg_edge_df.subject,
+                                                   self.neg_edge_df.object))))
         self.assertTrue(set(neg_nodes) <= set(unique_node_ids),
                         "Some nodes from negative edges are not in the nodes tsv file")
 
-        # test node_types
+
+    # TODO - test node_types fxn
 
 
