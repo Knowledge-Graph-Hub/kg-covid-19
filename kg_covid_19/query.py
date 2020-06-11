dd8e5c04125529fc8692f7019727ffb491961ac7
Justin Reese <justaddcoffee+github@gmail.com>
2020-06-11 14:57:59 -0700

    Remove old code

--- kg_covid_19/query.py
+++ kg_covid_19/query.py
@@ -1,11 +1,6 @@
 import logging
 
-QUERIES = {
-    'TargetCandidates': ''
-}
-
 
 def run_query(query: str, input_dir: str, output_dir: str) -> None:
     logging.info(f"Running query {query}")
-    t = QUERIES[query](input_dir, output_dir)
-    t.run()
+    pass
