4afc52946d338b20966458e402b64b1fc4c2c775
Justin Reese <justaddcoffee+github@gmail.com>
2020-06-16 20:38:40 -0700

    Moved code to write/edges for xref to Uniprot IDs

--- kg_covid_19/transform_utils/string_ppi/string_ppi.py
+++ kg_covid_19/transform_utils/string_ppi/string_ppi.py
@@ -213,25 +213,25 @@ class StringTransform(Transform):
                                       protein_node_type, "", "", self.source_name]
                             )
 
-                        # if we have an equivalent Uniprot ID for this Ensembl protein
-                        # ID make an xref edge, and a node for the Uniprot ID
-                        if nat_string_id in string_to_uniprot_id_map:
-                            uniprot_curie = \
-                                f"UniprotKB:{string_to_uniprot_id_map[nat_string_id]}"
-                            write_node_edge_item(
-                                fh=node,
-                                header=self.node_header,
-                                data=[uniprot_curie, "",
-                                      protein_node_type, "", "", self.source_name])
-                            write_node_edge_item(
-                                fh=edge,
-                                header=self.edge_header,
-                                data=[f"ENSEMBL:{protein}",
-                                      "biolink:xrefs",
-                                      uniprot_curie,
-                                      "biolink:xrefs",
-                                      "uniprot",
-                                      ] + self.extra_header)
+                    # if we have an equivalent Uniprot ID for this Ensembl protein
+                    # ID make an xref edge, and a node for the Uniprot ID
+                    if nat_string_id in string_to_uniprot_id_map:
+                        uniprot_curie = \
+                            f"UniprotKB:{string_to_uniprot_id_map[nat_string_id]}"
+                        write_node_edge_item(
+                            fh=node,
+                            header=self.node_header,
+                            data=[uniprot_curie, "",
+                                  protein_node_type, "", "", self.source_name])
+                        write_node_edge_item(
+                            fh=edge,
+                            header=self.edge_header,
+                            data=[f"ENSEMBL:{protein}",
+                                  "biolink:xrefs",
+                                  uniprot_curie,
+                                  "biolink:xrefs",
+                                  "uniprot",
+                                  ] + self.extra_header)
 
                 # write edge data
                 write_node_edge_item(
