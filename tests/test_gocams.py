75d879c0cf31d3a834aef0021e49dca3de8c4996
Justin Reese <justaddcoffee@gmail.com>
2021-06-09 16:05:50 -0700

    Add back a test

--- tests/test_gocams.py
+++ tests/test_gocams.py
@@ -17,35 +17,11 @@ class TestGOCams(unittest.TestCase):
         self.gocams_t = GocamTransform(input_dir=self.input_dir,
                                        output_dir=self.output_dir)
 
+    def test_run(self):
         # Suppress chatter
         with mock.patch('sys.stdout', new=io.StringIO()) as std_out:
             self.gocams_t.run(data_file=self.gc_nt_file)
             sys.stdout = sys.__stdout__
 
-        self.nodes_edges_output_dir = os.path.join(self.output_dir, "gocams")
-        self.expected_nodes_file = os.path.join(self.nodes_edges_output_dir, 'GOCAMs_nodes.tsv')
-        # self.expected_edges_file = os.path.join(self.nodes_edges_output_dir, 'GOCAMs_edges.tsv')
-        # self.expected_num_nodes = 3682
-        # self.expected_num_edges = 4161
-
-    def test_reality(self):
-        self.assertTrue(True)
-
     def test_run_exists(self):
         self.assertTrue(isinstance(self.gocams_t.run, object))
-
-    # def test_makes_output_dir(self):
-    #     self.assertTrue(os.path.isdir(self.output_dir))
-
-    def test_nodes_file_exists(self):
-        self.assertTrue(os.path.isfile(self.expected_nodes_file))
-    #
-    # def test_num_transformed_nodes(self):
-    #     self.assertTrue(sum(1 for line in open(self.expected_nodes_file)) > 1)
-    #
-    # def test_edges_file_exists(self):
-    #     self.assertTrue(os.path.isfile(self.expected_edges_file))
-    #
-    # def test_num_transformed_edges(self):
-    #     self.assertTrue(sum(1 for line in open(self.expected_edges_file)) > 1)
-
