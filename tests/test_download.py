7430537d443ca5c1f6724505b27e3541be005afc
Justin Reese <justaddcoffee@gmail.com>
2021-07-19 22:42:23 -0400

    Add test for api

--- tests/test_download.py
+++ tests/test_download.py
@@ -1,7 +1,9 @@
 import os
 import tempfile
 from unittest import TestCase, mock
+
 from kg_covid_19 import download
+from kg_covid_19.utils.download_utils import download_from_api, download_from_yaml
 
 
 class TestDownload(TestCase):
@@ -10,8 +12,11 @@ class TestDownload(TestCase):
     def setUp(self) -> None:
         pass
 
+    # @mock.patch('requests.get')
     def test_api_download(self):
-        pass
+        td = tempfile.gettempdir()
+        download_from_yaml('tests/resources/download/download_api.yaml', td,
+                           ignore_cache=True)
 
     # @mock.patch('requests.get')
     # def test_download(self, mock_get):
