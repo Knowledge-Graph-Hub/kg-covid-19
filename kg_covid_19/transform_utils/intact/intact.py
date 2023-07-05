92b434495fda9bfb0deb3028d4ab193344144d60
caufieldjh <j.harry.caufield@gmail.com>
2023-07-05 15:53:41 -0400

    Ignore a couple flags

--- kg_covid_19/transform_utils/intact/intact.py
+++ kg_covid_19/transform_utils/intact/intact.py
@@ -10,8 +10,7 @@ from typing import Dict, List, Optional, Union
 from xml.dom import minidom  # type: ignore
 
 from kg_covid_19.transform_utils.transform import Transform
-from kg_covid_19.utils.transform_utils import (unzip_to_tempdir,
-                                               write_node_edge_item)
+from kg_covid_19.utils.transform_utils import unzip_to_tempdir, write_node_edge_item
 
 """
 Ingest IntAct protein/protein interaction data
@@ -99,7 +98,6 @@ class IntAct(Transform):
         os.makedirs(self.output_dir, exist_ok=True)
 
         with open(output_node_file, "w") as node, open(output_edge_file, "w") as edge:
-
             # write node.tsv header
             node.write("\t".join(self.node_header) + "\n")
             edge.write("\t".join(self.edge_header) + "\n")
@@ -170,7 +168,7 @@ class IntAct(Transform):
             interaction_type_str = (
                 interaction_type[0]
                 .getElementsByTagName("shortLabel")[0]
-                .firstChild._data
+                .firstChild._data  # type: ignore
             )
 
             participants = interaction.getElementsByTagName("participant")
@@ -353,7 +351,7 @@ class IntAct(Transform):
             # interaction detection method
             try:
                 method = experiment.getElementsByTagName("interactionDetectionMethod")
-                label = method[0].getElementsByTagName("shortLabel")[0].firstChild.data
+                label = method[0].getElementsByTagName("shortLabel")[0].firstChild.data  # type: ignore
                 exp_dict[exp_id]["detection_method"] = label
             except (KeyError, IndexError, AttributeError):
                 pass
